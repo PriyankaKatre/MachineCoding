a36790ace88b8e614091d253d62526cef9529490
Priyanka Katre <priyanka.katre@greenmangaming.com>
2024-09-10 18:48:27 +0530

    pagination backend driven

--- usingReact/pagination_BE_Driven/src/App.tsx
+++ usingReact/pagination_BE_Driven/src/App.tsx
@@ -2,57 +2,68 @@ import { useState } from "react";
 import "./App.css";
 import useFetch from "./hooks/useFetch";
 
+// fetch('https://dummyjson.com/products?limit=10&skip=10&select=title,price')
+// .then(res => res.json())
+// .then(console.log);
+
 function App() {
-    const data = useFetch("https://dummyjson.com/products").apiResponse;
-    const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
+  const { data, totalPages } = useFetch(
+    `https://dummyjson.com/products?limit=10&skip=${page * 10 - 10}`
+  );
 
-    const handleSelectedPage = (selectedPage) => {
-        if (
-          selectedPage >= 1 &&
-          selectedPage <= data?.products.length/10 &&
-          selectedPage !== page
-        ) {
-          setPage(selectedPage);
-        }
+  {
+    data && data.products && console.log("data", totalPages);
+  }
+
+  const handleSelectedPage = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= totalPages &&
+      selectedPage !== page
+    ) {
+      setPage(selectedPage);
     }
+  };
 
-    if (!data?.products) {
-        return null
-    }
+  if (!data) {
+    return null;
+  }
 
-    return (
-      <>
-        <div className="products">
-          {data &&
-            data?.products.length > 0 &&
-            data?.products.slice(page * 10 - 10, page * 10).map((prod) => {
+  return (
+    <>
+      <div className="products">
+        {data &&
+          data?.products.length > 0 &&
+          data?.products.map((prod) => {
+            return (
+              <span className="products__single" key={prod.id}>
+                <img src={prod.thumbnail} alt={prod.title} />
+                <span>{prod.title}</span>
+              </span>
+            );
+          })}
+      </div>
+      <div>
+        {data && data?.products && (
+          <div className="pagination">
+            <span onClick={() => handleSelectedPage(page - 1)}>⏮️</span>
+            {[...Array(totalPages)].map((_, i) => {
               return (
-                <span className="products__single" key={prod.id}>
-                  <img src={prod.thumbnail} alt={prod.title} />
-                  <span>{prod.title}</span>
+                <span
+                  className={page === i + 1 ? "pagination__selected" : ""}
+                  onClick={() => handleSelectedPage(i + 1)}
+                >
+                  {i + 1}
                 </span>
               );
             })}
-
-          {data?.products && (
-            <div className="pagination">
-              <span onClick={() => handleSelectedPage(page- 1)}>⏮️</span>
-              {[...Array(data?.products.length / 10)].map((_, i) => {
-                return (
-                  <span
-                    className={page === i + 1 ? "pagination__selected" : ""}
-                    onClick={() => handleSelectedPage(i + 1)}
-                  >
-                    {i + 1}
-                  </span>
-                );
-              })}
-              <span onClick={() => handleSelectedPage(page + 1)}>⏭️</span>
-            </div>
-          )}
-        </div>
-      </>
-    );
+            <span onClick={() => handleSelectedPage(page + 1)}>⏭️</span>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default App;
--- usingReact/pagination_BE_Driven/src/hooks/useFetch.ts
+++ usingReact/pagination_BE_Driven/src/hooks/useFetch.ts
@@ -1,22 +1,25 @@
 import { useEffect, useState } from "react"
 
 const useFetch = (apiUrl) => {
-    const [apiResponse, setApiResponse] = useState();
-
-    const fetchData = async() => {
-        try {
-          const response = await fetch(apiUrl);
-          const data = await response.json();
-          data && setApiResponse(data);
-        } catch (error) {
-          console.error("Error fetching data:", error);
+    const [data, setData] = useState();
+    const [totalPages, setTotalPages] = useState(0);
+    const fetchData = async () => {
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data && data.products) {
+          setData(data);
+          setTotalPages(Math.ceil(data.total/10));
         }
-    }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
     useEffect(() => {
-        if (apiUrl) fetchData();
+      if (apiUrl) fetchData();
     }, [apiUrl]);
-    return { apiResponse };
+    return { data, totalPages };
 
 }
 
